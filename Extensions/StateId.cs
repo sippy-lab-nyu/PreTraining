be7a1c670f85260531308515bcfba2c0369aa53b
sahil-suresh <67023799+sahil-suresh@users.noreply.github.com>
2021-03-01 22:18:55 -0500

    Add files via upload

---
+++ Extensions/StateId.cs
@@ -0,0 +1,22 @@
+public enum StateId
+{
+    ITI,
+    Go,
+    NoGo,
+    Response,
+    Timeout,
+
+    Annotation,
+
+    Joystick,
+    Lick,
+    Blink,
+
+
+    Hit,
+    Miss,
+    FalseAlarm,
+    CorrectRejection,
+    PullPenalty,
+    EarlyResponse
+}
